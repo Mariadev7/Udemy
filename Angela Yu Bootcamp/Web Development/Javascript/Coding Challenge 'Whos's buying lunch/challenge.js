6198b9a23fb1c10baa85b28a985542156ff6fb05
Mariadev7 <73849264+Mariadev7@users.noreply.github.com>
2021-01-13 20:26:09 +0200

    Lunch

--- Angela Yu Bootcamp/Web Development/Javascript/Coding Challenge 'Whos's buying lunch/challenge.js
+++ Angela Yu Bootcamp/Web Development/Javascript/Coding Challenge 'Whos's buying lunch/challenge.js
@@ -1,7 +1,9 @@
 let names = ["Maria", "Jim", "George", "Teo", "Tereza", "Irene"];
 
-function lunch(){
-   let select= Math.random(names)
-    
-
+function lunch() {
+    let select = names[Math.floor(Math.random() * names.length)]
+    console.log(`${select} is going to pay for lunch`)
 }
+
+
+lunch()
---
+++ Angela Yu Bootcamp/Web Development/Javascript/Loops/loops.js
@@ -0,0 +1,10 @@
+let beer = 100;
+
+function song() {
+    while (beer >= 0) {
+        console.log(`${beer} bottles of beer on the wall, ${beer} bottles of beer. Take 1 down, pass it around, ${beer-1} bootles of beer on the wall`);
+        beer--
+    
+    }  
+} 
+song()
