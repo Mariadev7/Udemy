d52e4b69abe977f650bf32b6c4a74a188799ea02
Mariadev7 <73849264+Mariadev7@users.noreply.github.com>
2021-01-13 20:43:47 +0200

    While Update

--- Angela Yu Bootcamp/Web Development/Javascript/Loops/loops.js
+++ Angela Yu Bootcamp/Web Development/Javascript/Loops/loops.js
@@ -1,9 +1,13 @@
 let beer = 100;
 
 function song() {
-    while (beer >= 0) {
-        console.log(`${beer} bottles of beer on the wall, ${beer} bottles of beer. Take 1 down, pass it around, ${beer-1} bootles of beer on the wall`);
+    while (beer > 0) {
+        let bootleWord = "bottles";
+        if (beer === 1) {
+            bootleWord = "bottle";
+        }
+        console.log(`${beer} ${bootleWord} of beer on the wall, ${beer}  ${bootleWord} of beer. Take 1 down, pass it around, ${beer - 1}  ${bootleWord} of beer on the wall`);
         beer--
-    }  
-} 
+    }
+}
 song()
