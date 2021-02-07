7aa252a1346e891bfe516efb3abc18c8afe28a91
Mariadev7 <73849264+Mariadev7@users.noreply.github.com>
2021-02-07 19:01:03 +0200

    Bootstrap

--- Angela Yu Bootcamp/Web Development/jQuery/javascript/script.js
+++ Angela Yu Bootcamp/Web Development/jQuery/javascript/script.js
@@ -1,4 +1,24 @@
 
-$('h1').text('Bye');
+$("a").attr("href", "https://www.yahoo.com");
 
-$('button')
+
+$('h1').click(function(){
+$('h1').css('color', 'pink')
+
+})
+
+$('button').click(function(){
+    $('h1').css('color', 'red')
+})
+
+$('body').keypress(function(event){
+    $('h1').text(event.key)
+})
+
+$('h1').on('mouseover', function(){
+    $('h1').css('color','blue')
+})
+
+$('h1').mouseover(function(){
+    $('h1').slideUp(3000).slideDown(3000)
+})
